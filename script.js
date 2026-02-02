1c5c417e9440a01bca25cbdec133f9941e7a9eb1
niedjaraujos <naraujos1007@hotmail.com>
2026-02-01 22:30:20 -0300

    menu hamburguer para mobile e tablet

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+//menu hamburguer
+
+const menu = document.querySelector('.menu-hamburguer');
+const navList = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.navbar__link');
+
+menu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navList.classList.remove('active');
+  });
+});
 // intersection observer para animações de scroll
