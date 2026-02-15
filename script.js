1bc9856d9dbbacd59f9f6fb02c2766b70ec06bff
niedjaraujos <naraujos1007@hotmail.com>
2026-02-15 15:28:51 -0300

    refactor: repaginar largura e outros estilos

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 //menu hamburguer
 
-const menu = document.querySelector('.menu-hamburguer');
-const navList = document.querySelector('.navbar');
-const navLinks = document.querySelectorAll('.navbar__link');
+const menu = document.querySelector(".menu-hamburguer");
+const navList = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".navbar__link");
 
-menu.addEventListener('click', () => {
-  navList.classList.toggle('active');
+menu.addEventListener("click", () => {
+  navList.classList.toggle("active");
   menu.classList.toggle("active");
 });
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    navList.classList.remove('active');
-  });
-});
 // intersection observer para animações de scroll
